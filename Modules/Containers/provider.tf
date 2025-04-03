8baab2ea9cf3d6f0d46bd3dd51b55ae02545be61
Chawla <prashant.chawla@dynatrace.com>
2025-04-03 15:56:24 -0400

    terraform modules

--- Modules/Containers/provider.tf
+++ Modules/Containers/provider.tf
@@ -1,19 +1,9 @@
-output "container_rule_id" {
-  description = "The ID of the container built-in rule"
-  value       = dynatrace_container_builtin_rule.container_rule.id
+terraform {
+  required_providers {
+    dynatrace = {
+      source  = "dynatrace-oss/dynatrace"
+      version = "~> 1.0"
+    }
+  }
 }
 
-output "container_registry_id" {
-  description = "The ID of the container registry"
-  value       = dynatrace_container_registry.container_registry.id
-}
-
-output "container_monitoring_rule_id" {
-  description = "The ID of the container monitoring rule"
-  value       = dynatrace_container_rule.container_monitoring_rule.id
-}
-
-output "container_technology_id" {
-  description = "The ID of the container technology"
-  value       = dynatrace_container_technology.container_technology.id
-}
--- Modules/Containers/readme.md
+++ Modules/Containers/readme.md
@@ -115,4 +115,9 @@ module "dynatrace_container" {
   winc = var.winc
 }
 ```
+## API Token Scopes
+This resource requires the API token scopes:
+- Read settings (`settings.read`)
+- Write settings (`settings.write`)
 
+Make sure your API token includes these scopes to successfully create and manage the Dynatrace resources.
