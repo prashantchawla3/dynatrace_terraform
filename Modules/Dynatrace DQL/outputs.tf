a2ade53615b8532148b6eb1da00de3dafd31c596
Chawla <prashant.chawla@dynatrace.com>
2025-04-03 14:37:51 -0400

    terraform modules

--- Modules/Dynatrace DQL/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "dql_id" {
-  description = "The ID of the DQL query"
-  value       = data.dynatrace_dql.example.id
-}
-
-output "dql_records" {
-  description = "The records returned by the DQL query"
-  value       = data.dynatrace_dql.example.records
-}
