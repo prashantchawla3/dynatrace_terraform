4bc511a5257428d7ecd4aa24d701109bdb60afdd
Chawla <prashant.chawla@dynatrace.com>
2025-04-10 09:11:41 -0400

    terraform modules

--- terraform.tfvars
+++ terraform.tfvars
@@ -129,3 +129,36 @@ metric_metadata_unit         = "percent"
 
 metric_query_metric_id       = "metric-func:slo.terraform-test"
 metric_query_metric_selector = "((100*(builtin:service.requestCount.server:filter(in(\"dt.entity.service\",entitySelector(\"type(SERVICE),mzId(0000000000000000000),serviceType(WEB_SERVICE,WEB_REQUEST_SERVICE)\"))):splitBy())/(builtin:service.requestCount.server:filter(in(\"dt.entity.service\",entitySelector(\"type(SERVICE),mzId(0000000000000000000),serviceType(WEB_SERVICE,WEB_REQUEST_SERVICE)\"))):splitBy())) - (95.0))"
+
+host_group_id     = "environment"
+aspnetcore        = false
+cf_appsmanagerjs  = false
+container         = false
+docker_pauseamd64 = false
+exe_bbs           = false
+exe_caddy         = false
+exe_schedular     = false
+exe_silkdaemon    = false
+go_static         = false
+node_nodegyp      = false
+
+process_availability_enabled        = true
+process_availability_name           = "Process Availability Rule"
+process_availability_rule_property  = "executable"
+process_availability_rule_condition = "$contains(svc)"
+process_availability_metadata_key   = "foo"
+process_availability_metadata_value = "bar"
+
+process_monitoring_host_group_id   = "HOST_GROUP-0000000000000000"
+process_monitoring_auto_monitoring = true
+
+process_monitoring_rule_enabled            = true
+process_monitoring_rule_mode               = "MONITORING_OFF"
+process_monitoring_rule_host_group_id      = "HOST_GROUP-0000000000000000"
+process_monitoring_rule_condition_item     = "APACHE_CONFIG_PATH"
+process_monitoring_rule_condition_operator = "STARTS"
+process_monitoring_rule_condition_value    = "foo-bar-x"
+
+process_visibility_enabled       = true
+process_visibility_max_processes = 80
+process_visibility_scope         = "environment"
--- variables.tf
+++ variables.tf
@@ -576,3 +576,117 @@ variable "metric_query_metric_selector" {
   description = "Query"
   type        = string
 }
+
+variable "host_group_id" {
+  type = string
+}
+
+variable "aspnetcore" {
+  type = bool
+}
+
+variable "cf_appsmanagerjs" {
+  type = bool
+}
+
+variable "container" {
+  type = bool
+}
+
+variable "docker_pauseamd64" {
+  type = bool
+}
+
+variable "exe_bbs" {
+  type = bool
+}
+
+variable "exe_caddy" {
+  type = bool
+}
+
+variable "exe_schedular" {
+  type = bool
+}
+
+variable "exe_silkdaemon" {
+  type = bool
+}
+
+variable "go_static" {
+  type = bool
+}
+
+variable "node_nodegyp" {
+  type = bool
+}
+
+variable "process_availability_enabled" {
+  type = bool
+}
+
+variable "process_availability_name" {
+  type = string
+}
+
+variable "process_availability_rule_property" {
+  type = string
+}
+
+variable "process_availability_rule_condition" {
+  type = string
+}
+
+variable "process_availability_metadata_key" {
+  type = string
+}
+
+variable "process_availability_metadata_value" {
+  type = string
+}
+
+variable "process_monitoring_host_group_id" {
+  type = string
+}
+
+variable "process_monitoring_auto_monitoring" {
+  type = bool
+}
+
+variable "process_monitoring_rule_enabled" {
+  type = bool
+}
+
+variable "process_monitoring_rule_mode" {
+  type = string
+}
+
+variable "process_monitoring_rule_host_group_id" {
+  type = string
+}
+
+variable "process_monitoring_rule_condition_item" {
+  type = string
+}
+
+variable "process_monitoring_rule_condition_operator" {
+  type = string
+}
+
+variable "process_monitoring_rule_condition_value" {
+  type = string
+}
+
+variable "process_visibility_enabled" {
+  type = bool
+}
+
+variable "process_visibility_max_processes" {
+  type = number
+}
+
+variable "process_visibility_scope" {
+  type = string
+}
+
+
