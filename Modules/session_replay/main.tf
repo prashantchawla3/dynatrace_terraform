3fab92885819f8a57df139ef6550db70a5e3a3c1
ruthkyalo <164624367+ruthkyalo@users.noreply.github.com>
2025-05-02 20:43:09 +0300

    Update main.tf

--- Modules/session_replay/main.tf
+++ Modules/session_replay/main.tf
@@ -1,3 +1,4 @@
+
 resource "dynatrace_session_replay_resource_capture" "this" {
   application_id                              = var.application_id
   enable_resource_capturing                   = var.enable_resource_capturing
@@ -13,10 +14,13 @@ resource "dynatrace_session_replay_web_privacy" "this" {
     playback_masking_preset  = var.playback_masking_preset
     recording_masking_preset = var.recording_masking_preset
 
-    recording_masking_allow_list_rules {
-      allow_list_rule {
-        css_expression = var.recording_masking_allow_list_rules_css_expression
-        target         = var.recording_masking_allow_list_rules_target
+    dynamic "recording_masking_allow_list_rules" {
+      for_each = var.recording_masking_allow_list_rules
+      content {
+        allow_list_rule {
+          css_expression = recording_masking_allow_list_rules.value.css_expression
+          target         = recording_masking_allow_list_rules.value.target
+        }
       }
     }
   }
