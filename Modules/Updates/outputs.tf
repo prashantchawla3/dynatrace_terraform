04994cbe863ae795f41366ba5be92f5bb3958bab
ruthkyalo <164624367+ruthkyalo@users.noreply.github.com>
2025-05-02 21:03:21 +0300

    Update outputs.tf

--- Modules/Updates/outputs.tf
+++ Modules/Updates/outputs.tf
@@ -1,15 +1,15 @@
 
 output "activegate_updates_id" {
   description = "The ID of the ActiveGate updates resource"
-  value       = dynatrace_activegate_updates.example.id
+  value       = dynatrace_activegate_updates.this.id
 }
 
 output "oneagent_updates_id" {
   description = "The ID of the OneAgent updates resource"
-  value       = dynatrace_oneagent_updates.example.id
+  value       = dynatrace_oneagent_updates.this.id
 }
 
 output "update_windows_id" {
   description = "The ID of the update windows resource"
-  value       = dynatrace_update_windows.example.id
+  value       = dynatrace_update_windows.this.id
 }
