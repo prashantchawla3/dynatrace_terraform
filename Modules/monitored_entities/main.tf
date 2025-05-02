87deb27c5e8a12c1b3e2e81f3b1a294ff94d0f56
ruthkyalo <164624367+ruthkyalo@users.noreply.github.com>
2025-05-02 20:32:49 +0300

    Update main.tf

--- Modules/monitored_entities/main.tf
+++ Modules/monitored_entities/main.tf
@@ -1,13 +1,32 @@
+
 resource "dynatrace_custom_device" "custom_device" {
   custom_device_id = var.custom_device_id
   display_name     = var.display_name
   config_url       = var.config_url
-  dns_names        = var.dns_names
   favicon_url      = var.favicon_url
   group            = var.group
-  ip_addresses     = var.ip_addresses
-  listen_ports     = var.listen_ports
   props            = var.props
   type             = var.type
   ui_based         = var.ui_based
+
+  dynamic "dns_names" {
+    for_each = var.dns_names
+    content {
+      value = dns_names.value
+    }
+  }
+
+  dynamic "ip_addresses" {
+    for_each = var.ip_addresses
+    content {
+      value = ip_addresses.value
+    }
+  }
+
+  dynamic "listen_ports" {
+    for_each = var.listen_ports
+    content {
+      value = listen_ports.value
+    }
+  }
 }
