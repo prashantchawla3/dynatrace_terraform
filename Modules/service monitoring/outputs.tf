184a2fd693b8e91452c37c8dcd250e0b002417ca
ruthkyalo <164624367+ruthkyalo@users.noreply.github.com>
2025-05-02 20:38:52 +0300

    Update outputs.tf

--- Modules/service monitoring/outputs.tf
+++ Modules/service monitoring/outputs.tf
@@ -1,12 +1,14 @@
-
 output "calculated_service_metric_id" {
-  value = dynatrace_calculated_service_metric.example.id
+  value       = dynatrace_calculated_service_metric.example.id
+  description = "ID of calculated service metric"
 }
 
 output "request_attribute_id" {
-  value = dynatrace_request_attribute.example.id
+  value       = dynatrace_request_attribute.example.id
+  description = "ID of request attribute"
 }
 
 output "request_naming_id" {
-  value = dynatrace_service_naming.example.id
+  value       = dynatrace_service_naming.example.id
+  description = "ID of service naming pattern"
 }
