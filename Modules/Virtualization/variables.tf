871cc9bb6845531c8991b45428693abd3a161e27
ruthkyalo <164624367+ruthkyalo@users.noreply.github.com>
2025-05-02 20:51:10 +0300

    Update variables.tf

--- Modules/Virtualization/variables.tf
+++ Modules/Virtualization/variables.tf
@@ -1,36 +1,24 @@
-
-variable "environment_url" { type = string }
-variable "dynatrace_client_id" { type = string }
-variable "dynatrace_client_secret" { type = string, sensitive = true }
-
-variable "enabled_vmware" {
-  description = "Enable or disable the VMware integration"
-  type        = bool
+variable "vmware_configs" {
+  description = "Map of VMware configurations for Dynatrace"
+  type = map(object({
+    enabled   = bool
+    ipaddress = string
+    label     = string
+    password  = string
+    username  = string
+    filter    = string
+  }))
 }
 
-variable "ipaddress" {
-  description = "IP address or hostname of vCenter or standalone ESXi"
-  type        = string
+variable "environment_url" {
+  type = string
 }
 
-variable "label" {
-  description = "Label for the connection"
-  type        = string
+variable "dynatrace_client_id" {
+  type = string
 }
 
-variable "password" {
-  description = "Password for vCenter or ESXi"
-  type        = string
-  sensitive   = true
-}
-
-variable "username" {
-  description = "Username for vCenter or ESXi"
-  type        = string
-}
-
-variable "filter" {
-  description = "Optional filter string"
-  type        = string
-  default     = ""
+variable "dynatrace_client_secret" {
+  type      = string
+  sensitive = true
 }
