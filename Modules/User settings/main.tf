40dbdcdeb111c9424a1609e87818e5fa47c65bfe
ruthkyalo <164624367+ruthkyalo@users.noreply.github.com>
2025-05-02 20:57:28 +0300

    Update main.tf

--- Modules/User settings/main.tf
+++ Modules/User settings/main.tf
@@ -1,12 +1,12 @@
-
-resource "dynatrace_user_settings" "example" {
-  auto_language = var.auto_language
-  auto_region   = var.auto_region
-  auto_theme    = var.auto_theme
-  auto_timezone = var.auto_timezone
-  language      = var.language
-  region        = var.region
-  theme         = var.theme
-  timezone      = var.timezone
-  scope         = var.scope
+resource "dynatrace_user_settings" "this" {
+  for_each       = var.user_settings
+  auto_language  = each.value.auto_language
+  auto_region    = each.value.auto_region
+  auto_theme     = each.value.auto_theme
+  auto_timezone  = each.value.auto_timezone
+  language       = each.value.language
+  region         = each.value.region
+  theme          = each.value.theme
+  timezone       = each.value.timezone
+  scope          = each.value.scope
 }
