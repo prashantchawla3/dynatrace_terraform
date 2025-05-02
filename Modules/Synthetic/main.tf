852c5c20ff1cb59e24113aef7129301dd2cb2860
ruthkyalo <164624367+ruthkyalo@users.noreply.github.com>
2025-05-02 21:21:35 +0300

    Update main.tf

--- Modules/Synthetic/main.tf
+++ Modules/Synthetic/main.tf
@@ -1,4 +1,3 @@
-
 resource "dynatrace_calculated_synthetic_metric" "example" {
   count              = var.enable_calculated_synthetic_metric ? 1 : 0
   name               = var.calculated_synthetic_metric_name
