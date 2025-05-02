18b57df3b6d0e93c51ca6b51a790467a11fa505f
ruthkyalo <164624367+ruthkyalo@users.noreply.github.com>
2025-05-02 21:02:47 +0300

    Update main.tf

--- Modules/Updates/main.tf
+++ Modules/Updates/main.tf
@@ -1,24 +1,27 @@
 
-resource "dynatrace_activegate_updates" "example" {
+resource "dynatrace_activegate_updates" "this" {
   auto_update = var.auto_update
   scope       = var.scope_activegate
 }
 
-resource "dynatrace_oneagent_updates" "example" {
+resource "dynatrace_oneagent_updates" "this" {
   scope          = var.scope_oneagent
   target_version = var.target_version
   update_mode    = var.update_mode
 }
 
-resource "dynatrace_update_windows" "example" {
+resource "dynatrace_update_windows" "this" {
   name       = var.name
   enabled    = var.enabled
   recurrence = var.recurrence
 
-  once_recurrence {
-    recurrence_range {
-      start = var.once_recurrence_start
-      end   = var.once_recurrence_end
+  dynamic "once_recurrence" {
+    for_each = var.recurrence == "ONCE" ? [1] : []
+    content {
+      recurrence_range {
+        start = var.once_recurrence_start
+        end   = var.once_recurrence_end
+      }
     }
   }
 }
